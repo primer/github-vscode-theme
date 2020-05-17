01696102e05a42464f220ba7d3324151f7998a21
Daniel Castillo <dcastillok@gmail.com>
2020-05-17 15:36:57 +0200

    parallelize writing files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const getTheme = require("./theme");
 
 const lightTheme = getTheme({
   style: "light",
   name: "GitHub Light",
 });
-fs.writeFileSync("./themes/light.json", JSON.stringify(lightTheme, null, 2));
 
 const darkTheme = getTheme({
   style: "dark",
   name: "GitHub Dark",
 });
-fs.writeFileSync("./themes/dark.json", JSON.stringify(darkTheme, null, 2));
+
+fs.mkdir("./themes").then(() => {
+  fs.writeFile("./themes/light.json", JSON.stringify(lightTheme, null, 2))
+  fs.writeFile("./themes/dark.json", JSON.stringify(darkTheme, null, 2))
+})
