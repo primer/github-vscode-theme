33ba3ed873e4a8e13560efab6922c6281fe9f083
simurai <simulus@gmail.com>
2021-01-21 16:23:47 +0900

    Reduce editorWhitespace.foreground contrast

--- src/theme.js
+++ src/theme.js
@@ -142,7 +142,7 @@ function getTheme({ theme, name }) {
       "editorLineNumber.activeForeground" : color.text.primary,
       "editorIndentGuide.background"      : color.border.secondary,
       "editorIndentGuide.activeBackground": color.border.primary,
-      "editorWhitespace.foreground"       : color.border.tertiary,
+      "editorWhitespace.foreground"       : themes({ light: scale.gray[3], dark: scale.gray[5], dimmed: scale.gray[5] }),
       "editorCursor.foreground"           : themes({ light: scale.blue[7], dark: scale.blue[2], dimmed: scale.blue[2] }),
 
       "editor.findMatchBackground"          : themes({ light: scale.yellow[4], dark: "#ffd33d44", dimmed: "#ffd33d44" }),
