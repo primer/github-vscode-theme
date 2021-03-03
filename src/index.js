78090916a8439c5f5ace723d2cd9046397321ae2
Daw-Chih Liou <dawochih.liou@gmail.com>
2021-03-03 11:50:34 +0100

    add high contrast dark theme

--- src/index.js
+++ src/index.js
@@ -11,9 +11,15 @@ const darkTheme = getTheme({
   name: "GitHub Dark",
 });
 
+const hcTheme = getTheme({
+  style: "hc",
+  name: "GitHub High Contrast",
+});
+
 fs.mkdir("./themes", { recursive: true })
   .then(() => Promise.all([
     fs.writeFile("./themes/light.json", JSON.stringify(lightTheme, null, 2)),
     fs.writeFile("./themes/dark.json", JSON.stringify(darkTheme, null, 2)),
+    fs.writeFile("./themes/hc-dark.json", JSON.stringify(hcTheme, null, 2)),
   ]))
   .catch(() => process.exit(1))
--- src/primer.js
+++ src/primer.js
@@ -14,6 +14,19 @@ function getColors(style) {
       }
     });
     return darkColors;
+  } else if (style === "hc") {
+    /* The array of light to dark colors are reversed to auto-generate dark theme */
+    const hcColor = {};
+    Object.entries(colors).forEach(([name, val]) => {
+      if (name === "black") {
+        hcColor.white = val;
+      } else if (name === "white") {
+        hcColor.black = val;
+      } else {
+        hcColor[name] = [...val].reverse();
+      }
+    });
+    return hcColor;
   } else {
     return colors;
   }
