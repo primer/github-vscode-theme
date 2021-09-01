eebc726102a6dd746c478903f8531ad842993810
simurai <simurai@github.com>
2021-09-01 17:18:43 +0900

    Add "Dark High Contrast" theme

--- src/colors.js
+++ src/colors.js
@@ -1,6 +1,7 @@
 const lightColors = require("@primer/primitives/dist/json/colors_v2/light.json");
 const darkColors = require("@primer/primitives/dist/json/colors_v2/dark.json");
 const dimmedColors = require("@primer/primitives/dist/json/colors_v2/dark_dimmed.json");
+const hcColors = require("@primer/primitives/dist/json/colors_v2/dark_high_contrast.json");
 
 function getColors(theme) {
   if (theme === "light") {
@@ -9,6 +10,8 @@ function getColors(theme) {
     return darkColors;
   } else if (theme === "dimmed") {
     return dimmedColors;
+  } else if (theme === "hc") {
+    return hcColors;
   }
 }
 
