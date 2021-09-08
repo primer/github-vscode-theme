229f23c38d4d3f51c73b594b8b5cac901bb662a6
simurai <simurai@github.com>
2021-09-08 11:56:44 +0900

    Update primer/primitives path

--- src/colors.js
+++ src/colors.js
@@ -1,7 +1,7 @@
-const lightColors = require("@primer/primitives/dist/json/colors_v2/light.json");
-const darkColors = require("@primer/primitives/dist/json/colors_v2/dark.json");
-const dimmedColors = require("@primer/primitives/dist/json/colors_v2/dark_dimmed.json");
-const hcColors = require("@primer/primitives/dist/json/colors_v2/dark_high_contrast.json");
+const lightColors = require("@primer/primitives/dist/json/colors/light.json");
+const darkColors = require("@primer/primitives/dist/json/colors/dark.json");
+const dimmedColors = require("@primer/primitives/dist/json/colors/dark_dimmed.json");
+const hcColors = require("@primer/primitives/dist/json/colors/dark_high_contrast.json");
 
 function getColors(theme) {
   if (theme === "light") {
