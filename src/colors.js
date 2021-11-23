696a0b095d0651d9c7bf61f1b837d3ada5008b8f
Maxime De Greve <maximedegreve@github.com>
2021-11-23 17:54:09 +0000

    Update version and preview

--- src/colors.js
+++ src/colors.js
@@ -1,9 +1,9 @@
 const lightColors = require("@primer/primitives/dist/json/colors/light.json");
 const darkColors = require("@primer/primitives/dist/json/colors/dark.json");
 const dimmedColors = require("@primer/primitives/dist/json/colors/dark_dimmed.json");
-const lightColorblindColors = require("@primer/primitives/dist/json/colors/light_protanopia.json");
+const lightColorblindColors = require("@primer/primitives/dist/json/colors/light_colorblind.json");
 const hcColors = require("@primer/primitives/dist/json/colors/dark_high_contrast.json");
-const darkColorblindColors = require("@primer/primitives/dist/json/colors/dark_protanopia.json");
+const darkColorblindColors = require("@primer/primitives/dist/json/colors/dark_colorblind.json");
 
 function getColors(theme) {
   if (theme === "light") {
