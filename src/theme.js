e5a3b9ccb2ccb2279e8c9c5bf598ae22ed693b2b
simurai <simurai@github.com>
2022-07-12 16:59:25 +0900

    Improve diff colors

--- src/theme.js
+++ src/theme.js
@@ -189,8 +189,10 @@ function getTheme({ theme, name }) {
       "editorGutter.addedBackground"   : color.success.muted,
       "editorGutter.deletedBackground" : color.danger.muted,
 
-      "diffEditor.insertedTextBackground": color.success.subtle,
-      "diffEditor.removedTextBackground" : color.danger.subtle,
+      "diffEditor.insertedLineBackground": lightDark(alpha(scale.green[1], 0.3), alpha(scale.green[5], 0.2)),
+      "diffEditor.insertedTextBackground": lightDark(alpha(scale.green[2], 0.4), alpha(scale.green[5], 0.3)),
+      "diffEditor.removedLineBackground" : lightDark(alpha(scale.red[1], 0.3), alpha(scale.red[5], 0.2)),
+      "diffEditor.removedTextBackground" : lightDark(alpha(scale.red[2], 0.4), alpha(scale.red[5], 0.3)),
 
       "scrollbar.shadow"                  : alpha(scale.gray[5], 0.2),
       "scrollbarSlider.background"        : alpha(scale.gray[4], 0.2),
