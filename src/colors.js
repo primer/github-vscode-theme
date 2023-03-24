b94fc76a966add940475a5f12757a3ff17c99e97
simurai <simurai@github.com>
2023-03-24 18:06:35 +0900

    Improve color contrast

--- src/colors.js
+++ src/colors.js
@@ -10,12 +10,30 @@ function getColors(theme) {
 
   switch(theme) {
     case "light":
+
+      // Temp override until Primitives is updated
+      lightColors.success.emphasis = "#2c8747";
+      lightColors.btn.primary.bg = lightColors.success.emphasis;
+      lightColors.btn.primary.hoverBg = lightColors.scale.green[5];
+      lightColors.fg.default = "#1f2328";
+      lightColors.fg.muted = "#656d76";
+
       return lightColors;
     case "light_high_contrast":
       return lightHighContrastColors;
     case "light_colorblind":
         return lightColorblindColors;
     case "dark":
+
+      // Temp override until Primitives is updated
+      darkColors.fg.default = "#e4e8ec";
+      darkColors.fg.muted = "#7a8490";
+      darkColors.accent.fg = "#3f84e4";
+      darkColors.severe.subtle = "rgba(219, 109, 40, 0.1)";
+      darkColors.danger.subtle = "rgba(248, 81, 73, 0.1)";
+      darkColors.done.subtle = "rgba(163, 113, 247, 0.1)";
+      darkColors.sponsors.subtle = "rgba(219, 97, 162, 0.1)";
+
       return darkColors;
     case "dark_high_contrast":
       return darkHighContrastColors;
